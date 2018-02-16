195fb532f430aab34ff7a5819d31eb3bbdd083f3
ZSPina <34932135+ZSPina@users.noreply.github.com>
2018-02-16 17:54:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,30 +11,18 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [
-                    'Adafruit-BBIO>=1.0.10',
-                    'google-cloud-datastore>=1.4.0',
-                    'google-cloud-storage>=1.6.0',
-                    'numpy>=1.13.3',
-                    'pyserial>=3.4',
-                    'PyBOMBS>=2.3.2',
-                    'python-smbus>=3.1.2'
-                    # TODO: Put package requirements here
-                ]
-
 setup(
-    name='wt_uhf_hub',
-    version='0.9.1.1',
-    long_description=readme + '\n\n' + history,
-    author="Zachary Pina",
-    author_email='zacharypina@gmail.com',
-    url='https://github.com/ZSPina/wt_uhf_hub',
-    packages=find_packages(exclude=['docs', 'tests', 'venv']),
-    include_package_data=True,
-    install_requires=requirements,
-    license="MIT license",
-    zip_safe=False,
-    keywords='wt_uhf_hub',
+    name                =   'wt_uhf_hub',
+    version             =   '0.9.2',
+    long_description    =   readme + '\n\n' + history,
+    author              =   "Zachary Pina",
+    author_email        =   'zacharypina@gmail.com',
+    url                 =   'https://github.com/ZSPina/wt_uhf_hub',
+    packages            =   find_packages(exclude=['docs', 'tests', 'venv']),
+    include_package_data=   True,
+    license             =   "MIT license",
+    zip_safe            =   False,
+    keywords            =   'wt_uhf_hub',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
